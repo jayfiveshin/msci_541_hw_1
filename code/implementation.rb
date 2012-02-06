92f679465712037b9a3071c9ee4535568b17c603
Joonha Shin <shin.joonha@gmail.com>
2012-02-05 22:05:28 -0500

    added tokenize method

--- code/implementation.rb
+++ code/implementation.rb
@@ -1,4 +1,4 @@
 load 'code/methods.rb'
 
 article = gzip_read("data/chosen_article.txt.gz")
-tokenize article
+puts tokenize article
--- code/methods.rb
+++ code/methods.rb
@@ -1,8 +1,31 @@
 require 'zlib'
-require 'tokenizer'
 
 # accepts name of gzip'd file to be read
 # returns text of the file
 def gzip_read(file_name)
   Zlib::GzipReader.open(file_name).read
+end
+
+def tokenize(string)
+  word  = ""
+  words = []
+  middle_of_tag = false
+  string.downcase.split("").each { |char| 
+    if char.match(">")
+      middle_of_tag = false
+    elsif middle_of_tag
+      next
+    elsif char.match("<")
+      middle_of_tag = true
+      next
+    elsif char.match(/[a-z0-9]/)
+      word += char
+    elsif word.empty?
+      next
+    else
+      words << word
+      word = ""
+    end
+  }
+  words
 end
