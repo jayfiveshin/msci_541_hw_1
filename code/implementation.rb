7db794e45d5402023e734605b2419159b3c31176
Joonha Shin <shin.joonha@gmail.com>
2012-02-06 01:03:40 -0500

    added calculate_df method

--- code/implementation.rb
+++ code/implementation.rb
@@ -1,6 +1,9 @@
 load 'code/methods.rb'
 
-article = gzip_read("data/chosen_article.txt.gz")
+article = read_gzip("data/chosen_article.txt.gz")
 terms = tokenize(article)
 tf = calculate_tf(terms)
-display_tf_table(tf)
+display_table(tf)
+dictionary = build_dictionary(tf)
+df = calculate_df(dictionary, "data/latimes_sample.dat.gz")
+display_table(df)
--- code/methods.rb
+++ code/methods.rb
@@ -2,7 +2,7 @@ require 'zlib'
 
 # accepts name of gzip'd file to be read
 # returns text of the file
-def gzip_read(file_name)
+def read_gzip(file_name)
   Zlib::GzipReader.open(file_name).read
 end
 
@@ -42,17 +42,58 @@ def calculate_tf(terms)
   tf
 end
 
+def calculate_df(dictionary, file_name)
+  df = {}
+  doc = ""
+  doc_count = 0
+  middle_of_doc = false
+  Zlib::GzipReader.open(file_name) { |string|
+    string.each { |line|
+      line.downcase!
+      if line.match("</doc>")
+        doc << line
+        dictionary.each { |term|
+          if doc.match(term)
+            if df[term].nil?
+              df[term] = 1
+            else
+              df[term] += 1
+            end
+          end
+        }
+        middle_of_doc = false
+        doc = ""
+        next
+      elsif middle_of_doc
+        doc << line
+        next
+      elsif line.match("<doc>")
+        doc_count += 1
+        puts doc_count
+        doc << line
+        middle_of_doc = true
+        next
+      end
+    }
+  }
+  df
+end
+
+def build_dictionary(tf_hash)
+  dictionary = tf_hash.keys
+end
+
 def sort_by_frequency(tf_hash)
   Hash[tf_hash.sort_by { |term, frequency| -1*frequency}]
 end
 
-def display_tf_table(tf_hash)
+def display_table(tf_hash)
   rank = 1
   puts "RANK\tTERM\t\tFREQUENCY"
   sort_by_frequency(tf_hash).each { |term, frequency|
-    if rank > 20
-      break
-    end
+    # if rank > 20
+    #   break
+    # end
     if term.length > 7
       puts "#{rank}\t#{term}\t#{frequency}"
     else
